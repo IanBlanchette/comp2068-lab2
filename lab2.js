de194d562c8427f5b99ebc2ece02bc0727db3f15
Ian Blanchete <ianjblanchette@gmail.com>
2019-05-14 19:49:26 -0400

    math working

--- lab2.js
+++ lab2.js
@@ -1,7 +1,29 @@
+const doMath = (method, x, y) =>
+{
+	switch(method.toLowerCase())
+	{	
+	case 'subtract':
+		return { result: x - y, operation: "-"}; 
+	case 'multiply':
+		return { result: x * y, operation: "x"} 
+	case 'divide':
+		return { result: x / y, operation: "/"}
+	case 'add':
+		return { result: x + y, operation: "+"}
+	default:
+		return 'This is an invalid option';
+	}
+};
+
 const handleHTTP = (request, response) =>
 {
-	const query = request.query; 
-	response.send(`x + y = result`);
-}
+	const query = request.query;
+	const x = parseInt(query.x);
+	const y = parseInt(query.y);
+	const method = query.method; 
+	const result = doMath(method, x, y);
+
+	response.send(`${x} ${result.operation} ${y} = ${result.result}`);
+};
 
 module.exports = handleHTTP;
