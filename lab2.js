41e7727ccbc43a723d0728bc89d50a0bdc964016
Ian Blanchete <ianjblanchette@gmail.com>
2019-05-14 19:59:23 -0400

    working with different methods

--- lab2.js
+++ lab2.js
@@ -15,12 +15,24 @@ const doMath = (method, x, y) =>
 	}
 };
 
+const validMethod = ['add', 'subtract', 'multiply', 'divide'];
+
 const handleHTTP = (request, response) =>
 {
 	const query = request.query;
 	const x = parseInt(query.x);
 	const y = parseInt(query.y);
 	const method = query.method; 
+
+	if (isNaN(x) || isNaN(y))
+	{
+		return response.send('Both x and y must be a number');
+	}
+
+	if(!method || !validMethod.includes(method.toLowerCase()))
+	{
+		return response.send('Method must be included');
+	}
 	const result = doMath(method, x, y);
 
 	response.send(`${x} ${result.operation} ${y} = ${result.result}`);
